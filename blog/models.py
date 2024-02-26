f144205bcc3bd3e98c2b8655cd193877baecd37b
Tariq Ebden <tebden16@gmail.com>
2024-02-26 14:57:47 +0000

    created comment model and meta with methods

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,52 @@ class Post(models.Model):
   content = models.TextField()
   created_on = models.DateTimeField(auto_now_add = True)
   status = models.IntegerField(choices=STATUS, default=0)
+  excerpt = models.TextField(blank=True)
+  updated_on = models.DateTimeField(auto_now = True)
+
+  class Meta:
+    ordering = ["created_on"]
+
+  def __str__(self):
+    return f"{self.title} | written by {self.author}"
+
+class Comment(models.Model):
+  post = models.ForeignKey(
+    Post, on_delete=models.CASCADE, related_name="comments"
+  )
+  author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="comment_author"
+  )
+  body = models.TextField()
+  approved = models.BooleanField(default=False)
+  created_on = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ["created_on"]
+  
+  def __str__(self):
+    return f"Comment {self.body} by {self.author}"
+
+# EXAMPLES:
+
+# class Event(models.Model):
+#   event_name = models.CharField(max_length = 200, unique = True)
+#   location = models.CharField(max_length = 200, unique = True)
+#   date = models.DateTimeField()
+
+#   def __str__(self):
+#     return self.event_name
+
+# class Ticket(models.Model):
+#   ticket_holder = models.ForeignKey(
+#     User, on_delete=models.CASCADE, related_name="user_tickets"
+#   )
+#   date_issued = models.DateTimeField(auto_now_add = True)
+#   event = models.ForeignKey(
+#     Event,
+#     on_delete=models.CASCADE,
+#     related_name="event_ticekts"
+#   )
+
+#   def __str__(self):
+#     return f"Ticekt for {self.ticket_holder}"
