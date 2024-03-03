c1848ca0d43a68806224d1d7bf45d86a44101fdf
Tariq Ebden <tebden16@gmail.com>
2024-03-03 15:03:18 +0000

    added comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -6,6 +7,16 @@ from .forms import CollaborateForm
 def about_me(request):
   about = About.objects.all().order_by('-updated_on').first()
   collaborate_form = CollaborateForm()
+
+  if request.method == "POST":
+    collaborate_form = CollaborateForm(data=request.POST)
+    if collaborate_form.is_valid:
+      collaborate_form.save()
+      messages.add_message(
+        request, messages.SUCCESS,
+        'Collaboration request received. It may take up to 2 working days for a response.'
+      )
+
 
   return render(
     request,
